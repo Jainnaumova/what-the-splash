862b57e599925b021acc5bd27de073732621c25f
Evgeniya Naumova <jainnaumova@gmail.com>
2019-11-04 13:02:09 -0500

    first commit

--- src/components/ImageGrid/ImageGrid.js
+++ src/components/ImageGrid/ImageGrid.js
@@ -1,18 +1,26 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
 
-import Button from '../Button';
-import Stats from '../Stats';
-import { loadImages } from '../../actions';
 import './styles.css';
 
+const key = '5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
+
 class ImageGrid extends Component {
+    state = {
+        images: [],
+    };
+
     componentDidMount() {
-        this.props.loadImages();
+        fetch(`https://api.unsplash.com/photos/?client_id=${key}&per_page=28`)
+            .then(res => res.json())
+            .then(images => {
+                this.setState({
+                    images,
+                });
+            });
     }
 
     render() {
-        const { isLoading, images, loadImages, error, imageStats } = this.props;
+        const { images } = this.state;
         return (
             <div className="content">
                 <section className="grid">
@@ -23,7 +31,6 @@ class ImageGrid extends Component {
                                 image.height / image.width,
                             )}`}
                         >
-                            <Stats stats={imageStats[image.id]} />
                             <img
                                 src={image.urls.small}
                                 alt={image.user.username}
@@ -31,30 +38,9 @@ class ImageGrid extends Component {
                         </div>
                     ))}
                 </section>
-                {error && <div className="error">{JSON.stringify(error)}</div>}
-                <Button
-                    onClick={() => !isLoading && loadImages()}
-                    loading={isLoading}
-                >
-                    Load More
-                </Button>
             </div>
         );
     }
 }
 
-const mapStateToProps = ({ isLoading, images, error, imageStats }) => ({
-    isLoading,
-    images,
-    error,
-    imageStats,
-});
-
-const mapDispatchToProps = dispatch => ({
-    loadImages: () => dispatch(loadImages()),
-});
-
-export default connect(
-    mapStateToProps,
-    mapDispatchToProps,
-)(ImageGrid);
+export default ImageGrid;
--- src/sagas/index.js
+++
@@ -1,8 +0,0 @@
-import { all } from 'redux-saga/effects';
-
-import imagesSaga from './imagesSaga';
-import statsSaga from './statsSaga';
-
-export default function* rootSaga() {
-    yield all([imagesSaga(), statsSaga()]);
-}
